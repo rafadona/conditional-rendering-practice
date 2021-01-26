27d1e8a6901100afa77a40ad292973bd06811ee5
Rafael Dona <rafa_dona@hotmail.com>
2021-01-26 16:22:44 -0300

    criei  input.jsx pra pegar os props e usar no form

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,11 +1,12 @@
 import React from "react";
+import Input from "./Input";
 
 function Form() {
   return (
     <form className="form">
-      <input type="text" placeholder="Username" />
-      <input type="password" placeholder="Password" />
-      <input type="password" placeholder="Confirm Password" />
+      <Input type="text" placeholder="Username" />
+      <Input type="password" placeholder="Password" />
+      <Input type="password" placeholder="Confirm Password" />
       <button type="submit">Register</button>
     </form>
   );
